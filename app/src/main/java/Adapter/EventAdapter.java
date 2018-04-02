c9d9c86368cbc28d061ab58bf03e9a532e125d42
Markus <hutnik.markus@uwlax.edu>
2018-04-02 13:53:06 -0500

    RecyclerView added

--- app/src/main/java/Adapter/EventAdapter.java
+++ app/src/main/java/Adapter/EventAdapter.java
@@ -4,8 +4,12 @@ import android.app.LauncherActivity;
 import android.content.Context;
 import android.support.v7.widget.RecyclerView;
 import android.util.EventLog;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.robertwais.pickupgames.R;
 
 import java.util.List;
 
@@ -21,28 +25,39 @@ public class EventAdapter extends RecyclerView.Adapter<EventAdapter.ViewHolder> 
     private List<ListItem> listItems;
 
     public EventAdapter(Context context, List listItem) {
-
+        this.context = context;
+        this.listItems = listItem;
     }
 
 
     @Override
-    public EventAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_row, parent, false);
+        return new ViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(EventAdapter.ViewHolder holder, int position) {
-
+        ListItem item = listItems.get(position);
+        holder.name.setText(item.getName());
+        holder.description.setText(item.getDescription());
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return listItems.size();
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
+
+        public TextView name;
+        public TextView description;
+
         public ViewHolder(View itemView) {
             super(itemView);
+
+            name = (TextView) itemView.findViewById(R.id.title);
+            description = (TextView) itemView.findViewById(R.id.description);
         }
     }
 }
--- app/src/main/java/com/example/robertwais/pickupgames/EventBoardActivity.java
+++ app/src/main/java/com/example/robertwais/pickupgames/EventBoardActivity.java
@@ -2,12 +2,41 @@ package com.example.robertwais.pickupgames;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import Adapter.EventAdapter;
+import Model.ListItem;
 
 public class EventBoardActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter adapter;
+    private List<ListItem> listItems;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_event_board);
+
+        recyclerView = (RecyclerView) findViewById(R.id.recyclerViewID);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        listItems = new ArrayList<>();
+        for(int i = 0; i < 15; i++) {
+             ListItem item = new ListItem(
+                     "Item" + (i+1),
+                     "Description"
+             );
+
+             listItems.add(item);
+        }
+
+        adapter = new EventAdapter(this, listItems);
+        recyclerView.setAdapter(adapter);
     }
 }
