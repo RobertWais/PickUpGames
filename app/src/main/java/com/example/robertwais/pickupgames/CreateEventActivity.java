4e79a9209beee49af7aae796c0f2ced8f251d29a
Markus <hutnik.markus@uwlax.edu>
2018-04-22 15:37:27 -0500

    fixed keyboard blocking edit text and created a better way to pick the time when creating event

--- app/src/main/java/com/example/robertwais/pickupgames/CreateEventActivity.java
+++ app/src/main/java/com/example/robertwais/pickupgames/CreateEventActivity.java
@@ -4,8 +4,11 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -22,12 +25,15 @@ public class CreateEventActivity extends AppCompatActivity {
     private EditText titleText;
     private EditText descText;
     private EditText timeText;
-    private Button createButton;
-    private Button cancelButton;
+    private Button createButton, cancelButton, addHrBtn, subHrBtn, addMinBtn, subMinBtn;
+    private TextView hourText, minText;
     private FirebaseDatabase fDatabase;
     private DatabaseReference dbRef;
     private FirebaseUser user;
     private FirebaseAuth auth;
+    private Spinner spinner;
+    private ArrayAdapter<CharSequence> adapter;
+    private String timeOption;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,11 +43,80 @@ public class CreateEventActivity extends AppCompatActivity {
         auth = FirebaseAuth.getInstance();
         user = auth.getCurrentUser();
 
+        hourText = findViewById(R.id.hour);
+        minText = findViewById(R.id.minute);
         titleText = findViewById(R.id.enterTitle);
         descText = findViewById(R.id.enterDescription);
         timeText = findViewById(R.id.time);
         createButton = findViewById(R.id.submit);
         cancelButton = findViewById(R.id.cancel);
+        addHrBtn = findViewById(R.id.addHour);
+        subHrBtn = findViewById(R.id.subHour);
+        addMinBtn = findViewById(R.id.addMin);
+        subMinBtn = findViewById(R.id.subMin);
+
+        timeOption = "";
+
+        spinner = findViewById(R.id.option);
+        adapter = ArrayAdapter.createFromResource(this, R.array.time_options, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                timeOption = adapterView.getItemAtPosition(i).toString();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
+        addHrBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int hr = Integer.parseInt(hourText.getText().toString());
+                if(hr == 12) hr = 1;
+                else hr++;
+                hourText.setText(hr + "");
+            }
+        });
+
+        subHrBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int hr = Integer.parseInt(hourText.getText().toString());
+                if(hr == 1) hr = 12;
+                else hr--;
+                hourText.setText(hr + "");
+            }
+        });
+
+        addMinBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int min = Integer.parseInt(minText.getText().toString());
+                if(min == 59) min = 0;
+                else min++;
+                String minString = min + "";
+                if(minString.length() == 1) minString = 0 + minString;
+                minText.setText(minString);
+            }
+        });
+
+        subMinBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int min = Integer.parseInt(minText.getText().toString());
+                if(min == 0) min = 59;
+                else min--;
+                String minString = min + "";
+                if(minString.length() == 1) minString = 0 + minString;
+                minText.setText(minString);
+            }
+        });
 
         fDatabase = FirebaseDatabase.getInstance();
         dbRef = fDatabase.getReference().child("Posts").push();
@@ -50,7 +125,9 @@ public class CreateEventActivity extends AppCompatActivity {
             createButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                dbRef.setValue(new Post("1:00AM", descText.getText().toString(), "Change later", titleText.getText().toString(), user.getUid()));
+                String time = hourText.getText().toString() + ":" + minText.getText().toString() + " " + timeOption;
+
+                dbRef.setValue(new Post("1:00AM", descText.getText().toString(), time, titleText.getText().toString(), user.getUid()));
 
                 Intent intent = new Intent(CreateEventActivity.this, EventBoardActivity.class);
                 Toast.makeText(CreateEventActivity.this, "Event Created", Toast.LENGTH_SHORT).show();
